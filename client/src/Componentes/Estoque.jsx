2f9a7341f19ddc428ced0a7ff8e73903a379c1e5
JulianaMB9 <julianamacb6@gmail.com>
2024-07-08 20:00:32 -0300

    Estoque

--- client/src/Componentes/Estoque.jsx
+++ client/src/Componentes/Estoque.jsx
@@ -1,21 +1,23 @@
 import * as React from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
+import Paper from "@mui/material/Paper";
+// import Typography from "@mui/material/Typography";
 
 function Estoque() {
   return (
-    <> 
-
-    <BarChart
-      series={[
-        { data: [35, 44, 24, 34] },
-      ]}
-      height={290}
-      xAxis={[{ data: ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4'], scaleType: 'band' }]}
-      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-    />
-
-    </>
-   
+    <Paper elevation={3} style={{ padding: "20px" }}>
+      <BarChart
+        series={[{ data: [35, 44, 24, 34] }]}
+        height={290}
+        xAxis={[
+          {
+            data: ["Produto 1", "Produto 2", "Produto 3", "Produto 4"],
+            scaleType: "band",
+          },
+        ]}
+        margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+      />
+    </Paper>
   );
 }
 
